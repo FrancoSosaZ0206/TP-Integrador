1254d99d5064eef266351e0cbc63a78ce2f8eafa
FrancoSosaZ0206 <franconahuelsosa0206@gmail.com>
2022-10-30 00:56:48 -0300

    IGNORAR

--- TPIntegrador_FINAL/TDAVehiculo.c
+++ TPIntegrador_FINAL/TDAVehiculo.c
@@ -102,8 +102,8 @@ bool vehiculosIguales(VehiculoPtr vehiculo1,VehiculoPtr vehiculo2)
     bool matchPatente = strcmp(getPatente(vehiculo1),getPatente(vehiculo2)) == 0;
 
     bool match = getTipoVehiculo(vehiculo1) == getTipoVehiculo(vehiculo2);
-    bool match = match && strcmp(getMarca(vehiculo1),getMarca(vehiculo2)) == 0;
-    bool match = match && strcmp(getModelo(vehiculo1),getModelo(vehiculo2)) == 0;
+    match = match && strcmp(getMarca(vehiculo1),getMarca(vehiculo2)) == 0;
+    match = match && strcmp(getModelo(vehiculo1),getModelo(vehiculo2)) == 0;
     return matchPatente && match;
 }
 
